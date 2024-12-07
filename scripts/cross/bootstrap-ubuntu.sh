3a110cd05542392d20489cd359b4bbce457dab96
yuri.yin <yincc8@chinatelecom.cn>
2024-12-07 10:51:03 +0800

    Update the vector

--- scripts/cross/bootstrap-ubuntu.sh
+++ scripts/cross/bootstrap-ubuntu.sh
@@ -2,6 +2,8 @@
 set -o errexit
 
 echo 'Acquire::Retries "5";' > /etc/apt/apt.conf.d/80-retries
+##
+sed -i 's@//.*archive.ubuntu.com@//mirrors.ustc.edu.cn@g' /etc/apt/sources.list
 
 apt-get update
 apt-get install -y \
@@ -10,10 +12,15 @@ apt-get install -y \
   wget
 
 # we need LLVM >= 3.9 for onig_sys/bindgen
+#
+#cat <<-EOF > /etc/apt/sources.list.d/llvm.list
+#deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-9 main
+#deb-src http://apt.llvm.org/xenial/ llvm-toolchain-xenial-9 main
+#EOF
 
 cat <<-EOF > /etc/apt/sources.list.d/llvm.list
-deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-9 main
-deb-src http://apt.llvm.org/xenial/ llvm-toolchain-xenial-9 main
+deb http://mirrors.tuna.tsinghua.edu.cn/llvm-apt/xenial/ llvm-toolchain-xenial-9 main
+#deb-src http://mirrors.tuna.tsinghua.edu.cn/llvm-apt/xenial/ llvm-toolchain-xenial-9 main
 EOF
 
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key| apt-key add -
